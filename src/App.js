bf7afa454b19d0735237b9bac439d50c2ab6edac
liamw <liam.weber@outlook.com>
2022-07-23 10:20:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import Mapp from './Map';
+
+const postMethod = () => {
+  var axios = require('axios');
+  var data = JSON.stringify({
+    "image_info": {
+      "lat": "46.283087592655484",
+      "lon": "7.539040105745088",
+      "image_url": "http://myimage.com"
+    }
+  });
+
+  var config = {
+    method: 'post',
+    url: 'https://lxhdude.hevs.ch:1423/knime/rest/v4/repository/00_Users/Jerome/VineLineRecognition:execution',
+    headers: {
+      'accept': 'application/vnd.mason+json',
+      'Authorization': 'Basic YXBpOmFwaQ==',
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+const getMethod = () => {
+  var axios = require('axios');
+
+  var config = {
+    method: 'get',
+    url: 'https://lxhdude.hevs.ch:1423/knime/rest/v4/repository/00_Users/Jerome/VineLineRecognition:execution',
+    headers: {
+      'accept': 'application/vnd.mason+json',
+      'Authorization': 'Basic YXBpOmFwaQ==',
+      'Content-Type': 'application/json'
+    }
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id='leftbox'>
+        <Mapp />
+      </div>
+      <div id='rightbox'>
+        <h1>Prototype TB</h1>
+        <br />
+        <button onClick={postMethod}>Send to API</button>
+        <button onClick={getMethod}>Receive from API</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Map.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiemFybWFzb2V1cmV0dGUiLCJhIjoiY2thZmRxcjN3MDNmYTJ1cThicXNoMHg0bSJ9.B2qltnoj3tDA3pHPSmSWnQ';
+
+class Mapp extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			lng: 6.945518553256988,
+			lat: 46.2545448844787,
+			zoom: 18
+		}
+	}
+
+	componentDidMount() {
+		// Set up map 
+		const map = new mapboxgl.Map({
+			container: this.mapContainer,
+			style: 'mapbox://styles/mapbox/satellite-v9',
+			center: [this.state.lng, this.state.lat],
+			zoom: this.state.zoom,
+		});
+
+		//Setting up the tools to draw on the map
+		const draw = new MapboxDraw({
+			displayControlsDefault: false,
+			controls: {
+				polygon: true,
+				line_string: true,
+				trash: true
+			},
+			defaultMode: 'draw_polygon'
+		});
+		map.addControl(new mapboxgl.NavigationControl());
+		map.addControl(draw);
+
+		//Get coordinates for the polygon we draw
+		map.on('draw.create', function (e) {
+			var x = draw.getAll();
+			console.log(x['features'][0]['geometry']['coordinates'][0]);
+		});
+	}
+
+	render() {
+		return (
+			<div className='map'>
+				<div ref={el => this.mapContainer = el} style={{ width: '100%', height: '100vh' }} />
+			</div>
+		)
+	}
+}
+
+export default Mapp;
